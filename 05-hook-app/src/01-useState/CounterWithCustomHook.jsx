d3f609c70add9bbfaae09ea6f17d4bc1b3e0fdd4
RaulCudris <raulcudriz@hotmail.com>
2023-12-20 18:42:27 -0500

    Actualizacion del Proyecto..

--- 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
+++ 05-hook-app/src/01-useState/CounterWithCustomHook.jsx
@@ -1,16 +1,16 @@
 import { useCounter } from "../hooks/useCounter";
 
 export const CounterWithCustomHook = () =>{
-    const {  counter } = useCounter();
+    const {  counter ,  increment, reset , decrement } = useCounter();
 
     return (
     <>
         <h1>Counter with Hook: { counter }</h1>
         <hr/>
 
-        <button className="btn btn-warning">+1</button>
-        <button className="btn btn-success">Reset</button>
-        <button className="btn btn-danger">-1</button>
+        <button className="btn btn-warning" onClick={ increment }>+1</button>
+        <button className="btn btn-success" onClick={ reset }>Reset</button>
+        <button className="btn btn-danger"  onClick={decrement}>-1</button>
 
     </>
   )
---
+++ 05-hook-app/src/03-examples/MultipleCustomHooks.jsx
@@ -0,0 +1,10 @@
+
+export const MultipleCustomHooks = () => {
+  return (
+    <>
+     <h1>BreakingBad Quotes</h1>
+     <hr/> 
+    </>
+    
+  )
+}
