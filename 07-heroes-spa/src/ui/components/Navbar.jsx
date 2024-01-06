8524d98ba8d99c2695ee6f2b24945a325b1435f6
RaulCudris <raulcudriz@hotmail.com>
2024-01-06 16:48:36 -0500

    Craecion de login

--- 07-heroes-spa/src/ui/components/Navbar.jsx
+++ 07-heroes-spa/src/ui/components/Navbar.jsx
@@ -1,9 +1,15 @@
+import { useContext } from "react";
 import { Link, NavLink, useNavigate } from "react-router-dom";
+import { AuthContext } from "../../auth";
 
 export const Navbar = (  ) => {
+
+  const { user, logout } = useContext(AuthContext);
+
   const navigate = useNavigate();
 
   const onLogout = () =>{
+    logout();
     navigate('/login',{
       replace: true
    });
@@ -42,12 +48,15 @@ export const Navbar = (  ) => {
 
       <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
         <ul className="navbar-nav ml-auto">
-          <span className="nav-item nav-link text-info">Raul Cudris</span>
+          <span className="nav-item nav-link text-info">
+            { user?.name }
+          </span>
+          
           <button 
             onClick={ onLogout }
             className="nav-item nav-link btn"
             >logout
-            </button>
+          </button>
         </ul>
       </div>
     </nav>
