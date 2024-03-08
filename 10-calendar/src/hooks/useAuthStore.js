20b832c6e908bc6ffb5b15ffe454d1cadc6dbfb7
RaulCudris <raulcudriz@hotmail.com>
2024-03-08 17:03:04 -0500

    Implementacion del Fronted

---
+++ 10-calendar/src/hooks/useAuthStore.js
@@ -0,0 +1,30 @@
+import { useDispatch, useSelector } from "react-redux";
+import { calendarApi } from "../api";
+
+ 
+ export const useAuthStore = () =>{
+    const { status, user , errorMessage } = useSelector( state => state.auth);
+    const dispatch = useDispatch();
+
+    const startLogin = async({ email, password })=>{
+        dispatch( onChecking() )
+       try {
+           const { data } = await calendarApi.post('/auth',{email ,password});
+           localStorage.setItem('token', data.token );
+           localStorage.setItem('token-init-date', new Date().getTime());
+           dispatch( onLogin({ name: data.name , uid: data.uid }));
+           
+        } catch (error) {
+            console.log({error})
+        }
+    }
+
+    return{
+        //*Propiedades
+        status,
+        user , 
+        errorMessage,
+        //* Metodos
+        startLogin,
+    }
+ }
